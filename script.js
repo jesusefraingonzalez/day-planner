ab258e676f3921e2ada50a78f276f492ea70e61f
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2020-11-13 01:31:30 -0500

    add save functionality to save button

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ var now = dayjs();
 var currentDay = $("#current-day");
 currentDay.text(now.format('dddd , DD  MMMM YYYY'));
 
+
+//add functionality for saving text in todo area
+//key = time of time block , value = textarea content
+var saveBtn = $(".saveBtn");
+saveBtn.on('click' , function(event){
+    event.preventDefault();
+    var userText = $('textarea').val();
+    localStorage.setItem('efrain' , userText);
+});
+
